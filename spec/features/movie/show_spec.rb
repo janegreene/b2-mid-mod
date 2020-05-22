534e809e21321c3696dbb19221e8f9c56578e835
Jane Greene <janegreenecasa@gmail.com>
2020-05-22 11:46:28 -0600

    added test for second portion of story 2

--- spec/features/movie/show_spec.rb
+++ spec/features/movie/show_spec.rb
@@ -10,13 +10,35 @@ RSpec.describe "the movie show page" do
     van = sony.movies.create(title:"Van Wilder", creation_year: 1999, genre: "Comedy" )
 
     visit "/movies/#{apollo.id}"
-  # save_and_open_page
-  expect(page).to have_content(apollo.title)
-  expect(page).to have_content(apollo.creation_year)
-  expect(page).to have_content(apollo.genre)
+    expect(page).to have_content(apollo.title)
+    expect(page).to have_content(apollo.creation_year)
+    expect(page).to have_content(apollo.genre)
+    expect(page).not_to have_content(van.title)
 
-      end
-    end
+  end
+ it "a list of all its actors from youngest to oldest" do
+    # it "average age of all of the movie's actors" do
+  sony = Studio.create(name:       "Sony",
+                     location:  "Hollywood, CA")
+
+  bill = Actor.create(name:  "Bill Paxton",
+                       age:      57)
+  tom = Actor.create(name: "Tom Hanks",
+                       age:      60)
+  kevin = Actor.create(name: "Kevin Bacon",
+                       age:      51)
+
+  apollo = Movie.create(title:"Apollo 13", creation_year: 1996, genre: "Drama", studio_id: sony.id )
+  ActorMovie.create(actor_id: tom.id, movie_id: apollo.id)
+  ActorMovie.create(actor_id: bill.id, movie_id: apollo.id)
+  ActorMovie.create(actor_id: kevin.id, movie_id: apollo.id)
+
+  visit "/movies/#{apollo.id}"
+
+  expect("Kevin Bacon").to appear_before("Bill Paxton")
+  expect("Bill Paxton").to appear_before("Tom Hanks")
+end
+end
   # end
 # end
 
