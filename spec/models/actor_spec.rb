7dade3d791c6806e6358b970d734d2a96ebe9e80
Jane Greene <janegreenecasa@gmail.com>
2020-05-22 11:53:25 -0600

    completed model test for age order of actors in story 2

--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -11,6 +11,26 @@ RSpec.describe Actor do
     # it {should have_many :studios}
   end
   describe "methods" do
+    it "#age_order" do
+      sony = Studio.create(name:       "Sony",
+                         location:  "Hollywood, CA")
+
+      bill = Actor.create(name:  "Bill Paxton",
+                           age:      57)
+      tom = Actor.create(name: "Tom Hanks",
+                           age:      60)
+      kevin = Actor.create(name: "Kevin Bacon",
+                           age:      51)
+
+      apollo = Movie.create(title:"Apollo 13", creation_year: 1996, genre: "Drama", studio_id: sony.id )
+      ActorMovie.create(actor_id: tom.id, movie_id: apollo.id)
+      ActorMovie.create(actor_id: bill.id, movie_id: apollo.id)
+      ActorMovie.create(actor_id: kevin.id, movie_id: apollo.id)
+
+
+      expect(Actor.age_order.first).to eq(kevin)
+      expect(Actor.age_order.last).to eq(tom)
+  end
   end
 end
 # rails g migration CreateActors name:string age:integer
